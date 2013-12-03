647bdaa795a2119d189da76912eb8b61789f5e0b
Lachlan <mtsneffels13@gmail.com>
2013-12-03 14:08:56 -0500

    Added hints attribute to player class.

--- reversi/Player.py
+++ reversi/Player.py
@@ -1,9 +1,11 @@
 class Player():
 
-	def __init__(self, shape, name):
-		#define name
-		self.name = name
-		#define shape
-		self.shape = shape
-		#define score
-		self.score = 0
+    def __init__(self, shape, name, hints):
+        #define name
+        self.name = name
+        #define shape
+        self.shape = shape
+        #define score
+        self.score = 0
+        self.hints = hints
+
